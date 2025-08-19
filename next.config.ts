384587ef87bd1778fe87b6b3d18b1d3cb87b61ce
sarnick005 <sarnick.chakraborty@gmail.com>
2025-08-19 23:04:41 +0530

    vercel config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,9 @@
 const nextConfig = {
   output: "standalone",
   experimental: {
-    serverActions: true,
+    serverActions: {},
   },
-  // This ensures static assets are properly served
-  distDir: ".next",
+  distDir: ".next", 
 };
 
 module.exports = nextConfig;
