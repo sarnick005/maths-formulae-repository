d80ccae26493a6e10c7c87b3c1e8fb0b918f2aab
sarnick005 <sarnick.chakraborty@gmail.com>
2025-09-25 11:32:36 +0530

    default next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  experimental: {
-    serverActions: true,
-  },
-  // This ensures static assets are properly served
-  distDir: ".next",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
